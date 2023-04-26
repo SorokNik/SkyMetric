061c6f6d4bb9266b0a01e3e1944d4da5199601f4
Sorokin Nikita <sorokin.nik@live.com>
2023-04-26 15:34:41 +0300

    mod hamburger & 320px view

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -1,11 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    body = document.body;
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+        body.classList.toggle('overflow-hidden');
     });
 
     menuItem.forEach(item => {
